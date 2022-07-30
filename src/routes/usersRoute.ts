94137f6f265cb9ffc9d3fade4d8ecca86cd125de
DeepeshDragoneel <deepeshash444@gmail.com>
2022-07-30 19:46:12 +0530

    Added signup feature

--- src/routes/usersRoute.ts
+++ src/routes/usersRoute.ts
@@ -7,6 +7,8 @@ import logging from "../config/logging";
 import { Connect, Query } from "../config/mysql";
 import { connection } from "../db";
 const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+import { SMTPClient } from "emailjs";
 
 type DomainResultType = { domainId: number; domainName: string };
 type UserResultType = {
@@ -145,25 +147,119 @@ router.post(
     }
 );
 
-router.post("/checkUser", (req: express.Request, res: express.Response) => {
-    const token = req.body.AnnotateJsUserToken;
-    logging.info("CHECK USER", "Checking user");
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
-        logging.info("CHECK USER", "User found");
-        res.status(200).json({
-            success: true,
-            message: "User found",
-            userName: decoded.userName,
-        });
-    } catch (err) {
-        logging.error("CHECK USER", err as string);
-        res.status(200).json({
-            success: false,
-            message: "User not found",
-        });
+router.post(
+    "/checkUser",
+    async (req: express.Request, res: express.Response) => {
+        const token = req.body.AnnotateJsUserToken;
+        logging.info("CHECK USER", "Checking user");
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+            logging.info("CHECK USER", "User found");
+            res.status(200).json({
+                success: true,
+                message: "User found",
+                userName: decoded.userName,
+            });
+        } catch (err) {
+            logging.error("CHECK USER", err as string);
+            res.status(200).json({
+                success: false,
+                message: "User not found",
+            });
+        }
     }
-});
+);
+
+router.post(
+    "/userRegister",
+    async (req: express.Request, res: express.Response) => {
+        const userName = req.body.userName;
+        const email = req.body.email;
+        const password = req.body.password;
+        logging.info("USER REGISTER", "User register");
+        try {
+            let query = "SELECT * FROM users WHERE email = '" + email + "'";
+            const results = (await Query(
+                connection!,
+                query
+            )) as UserResultType[];
+            logging.info("USER REGISTER", "User: ", results);
+            if (results.length === 0 || results === undefined) {
+                logging.info("USER REGISTER", "Adding User");
+                // query = `INSERT INTO users (userName, email, password) VALUES ('${userName}', '${email}', '${password}')`;
+                // await Query(connection!, query);
+                // logging.info("USER REGISTER", "User added");
+                // res.status(200).json({
+                //     success: true,
+                //     message: "User added",
+                // });
+                const token = jwt.sign(
+                    {
+                        userName: userName,
+                        email: email,
+                        password: password,
+                    },
+                    process.env.JWT_SECRET_KEY
+                );
+                const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+                console.log(decoded);
+                console.log(token);
+                let transporter = nodemailer.createTransport({
+                    service: "Gmail",
+                    auth: {
+                        user: process.env.GMAIL,
+                        pass: process.env.GMAIL_PASSWORD,
+                    },
+                });
+                let info = await transporter.sendMail({
+                    from: "annotatejs@gmail.com",
+                    to: email,
+                    subject: "AnnotateJS Registration",
+                    text: "Dont share this link to anyone!",
+                    html: `<h2>Please click on the below link to verfiy your Account</h2><br/><a href=${process.env.BACKEND_URL}/verifyUser?token=${token}>Verify Account</a>`,
+                });
+                res.status(200).json({
+                    success: true,
+                    message: "Email sent to the user",
+                });
+            } else {
+                logging.info("USER REGISTER", "User already exists");
+                res.status(200).json({
+                    success: false,
+                    message: "User already exists",
+                });
+            }
+        } catch (err) {
+            logging.error("USER REGISTER", err as string);
+        }
+    }
+);
+
+router.get(
+    "/verifyUser",
+    async (req: express.Request, res: express.Response) => {
+        const token = req.query.token!;
+        console.log(token);
+        logging.info("VERIFY USER", "Verifying user");
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+            logging.info("VERIFY USER", "Adding user");
+            const query = `INSERT INTO users (userName, email, password) VALUES ('${decoded.userName}', '${decoded.email}', '${decoded.password}')`;
+            await Query(connection!, query);
+            logging.info("VERIFY USER", "User added");
+            res.set('Content-Type', 'text/html');
+            res.send(
+                Buffer.from(
+                    "<h1>User added</h1><br/><h3>You can now Login with your credentials!</h3>"
+                )
+            );
+        } catch (err) {
+            logging.error("VERIFY USER", err as string);
+            res.set('Content-Type', 'text/html');
+            res.send(Buffer.from("<h1>Error Adding User</h1>"));
+        }
+    }
+);
 
 router.get("/", (req: express.Request, res: express.Response) => {
     res.send("Hello World!");
